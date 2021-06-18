28ee28c6fb4ade747a06ef18aee4aab6e994292d
Andrey Belskiy <anb@smena.space>
2021-06-18 16:08:39 +0500

    Added some CRUD/mutations functionality

--- quiz/admin.py
+++ quiz/admin.py
@@ -5,6 +5,7 @@ from . import models
 @admin.register(models.Category)
 class CatAdmin(admin.ModelAdmin):
     list_display = [
+        'id',
         'name',
     ]
 
--- quiz/schema.py
+++ quiz/schema.py
@@ -33,6 +33,7 @@ class Query(graphene.ObjectType):
 
     all_questions = graphene.Field(QuestionType, id=graphene.Int())
     all_answers = graphene.List(AnswerType, id=graphene.Int())
+    all_categories = graphene.List(CategoryType)
     # all_quizzes = DjangoListField(QuizzesType)
     all_quizzes = graphene.List(QuizzesType)
 
@@ -44,11 +45,48 @@ class Query(graphene.ObjectType):
     def resolve_all_answers(root, info, id):
         return Answer.objects.filter(question=id)
 
-    def resolve_all_quizzes(root, into):
+    def resolve_all_quizzes(root, info):
         return Quizzes.objects.filter(pk=2)
+
+    def resolve_all_categories(root, info):
+        return Category.objects.all()
 
     def resolve_quiz(root, info):
         return f"This is some line"
 
 
-schema = graphene.Schema(query=Query)
+class CategoryMutation(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+
+    category = graphene.Field(CategoryType)
+
+    @classmethod
+    def mutate(cls, root, info, name):
+        category = Category(name=name)
+        category.save()
+        return CategoryMutation(category=category)
+
+
+class CategoryRenaming(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        name = graphene.String(required=True)
+
+    category = graphene.Field(CategoryType)
+
+    @classmethod
+    def mutate(cls, root, info, name, id):
+        category = Category.objects.get(pk=id)
+        category.name = name
+        category.save()
+        # category.delete()     # For deleting
+        return CategoryMutation(category=category)
+
+
+class Mutation(graphene.ObjectType):
+
+    update_category = CategoryMutation.Field()
+    rename_category = CategoryRenaming.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
